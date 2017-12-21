731dc0f0278f66dfe3b0d1d336491982c90758ab
Pedro Navarrete Hurtado <navhpedro@gmail.com>
2017-12-20 21:42:42 -0600

    Testing Button

--- HelloWorld/HelloWorld/GreetPage.xaml.cs
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -12,9 +12,17 @@ namespace HelloWorld
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class GreetPage : ContentPage
 	{
+
 		public GreetPage ()
 		{
 			InitializeComponent ();
 		}
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Title", "Hello World", "OK");
+        }
+
+
 	}
 }
