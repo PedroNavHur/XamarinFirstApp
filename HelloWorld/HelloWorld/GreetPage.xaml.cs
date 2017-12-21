0e37b78c0228e6dd304f6d180959130ce58d6f01
Pedro Navarrete Hurtado <navhpedro@gmail.com>
2017-12-21 15:32:21 -0600

    Padding

--- HelloWorld/HelloWorld/GreetPage.xaml.cs
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -9,22 +9,30 @@ using Xamarin.Forms.Xaml;
 
 namespace HelloWorld
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class GreetPage : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetPage : ContentPage
+    {
 
-		public GreetPage ()
-		{
-			InitializeComponent ();
+        public GreetPage()
+        {
+            InitializeComponent();
             slider.Value = 0.5;
-		}
+
+            if (Device.RuntimePlatform == Device.iOS)
+                Padding = new Thickness(0, 20, 0, 0);
+            else if (Device.RuntimePlatform == Device.Android)
+                Padding = new Thickness(0, 0, 0, 0);
+            else if  (Device.RuntimePlatform == Device.UWP)
+                Padding = new Thickness(0, 0, 0, 0);
+
+        }
 
         /* Slider Handler */
         //void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
         //{
         //    label.Text = String.Format("Value is {0:F2}", e.NewValue);
         //}
-        
+
         /* Button Handler */
         //void Handle_Clicked(object sender, System.EventArgs e)
         //{
@@ -32,5 +40,5 @@ namespace HelloWorld
         //}
 
 
-	}
+    }
 }
